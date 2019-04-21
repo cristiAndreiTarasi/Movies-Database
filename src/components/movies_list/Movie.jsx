f94c1ce71243a301f4a71d78c9842ddb01d5509f
Cristi Tarasi <tarasicristia@gmail.com>
2019-04-21 19:48:37 +0100

    fixed the modal behaviour, which wasn't showing up after switching to hooks

--- src/components/movies_list/Movie.jsx
+++ src/components/movies_list/Movie.jsx
@@ -1,90 +1,17 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import gen from '../../assets/genres.js';
 import { BASE_URL, API_KEY, DISCOVER, IMG_URL, YOUTUBE_LINK } from '../../assets/api_bits.js';
 import MovieModal from './MovieModal.jsx';
 
-export default class extends Component {
-    state = { 
-        showModal: false, 
-        video: YOUTUBE_LINK
-    }; 
-
-    handleOpenModal = () => {
-        this.setState({ showModal: true });
-    }
-
-    handleCloseModal = () => {
-        this.setState({ showModal: false });
-    }
-
-    componentDidMount () {
-        const get_movie_call = async () => {
-            const movie = await fetch(`${BASE_URL}/movie/${this.props.data.id}/videos${API_KEY}&language=en-US`);
-            const json = await movie.json();
-
-            this.setState({ video: this.state.video.concat(json.results[0].key) })
-            console.log(this.state.video);
-        }
-
-        get_movie_call();
-    }
-
-    render () {
-        return (
-            <Fragment>
-                <div 
-                    className='panel'
-                    onClick={this.handleOpenModal}
-                >
-                    <img className="panel-img" src={`${IMG_URL}` + this.props.data.poster_path} alt={this.props.data.title} />
-                    <div className="panel-body">
-                        <h3 className='panel-title'>
-                            { (this.props.data.title || this.props.data.original_name).length >= 24 
-                                ? (this.props.data.title || this.props.data.original_name).slice(0, 24).concat('...') 
-                                : this.props.data.title || this.props.data.original_name }
-                        </h3>
-                        <h4 className="panel-genres">
-                            {gen.map (genre => (
-                                this.props.data.genre_ids.includes(genre.id) 
-                                    ? `${genre.name}\xa0\xa0` 
-                                    : null
-                            ))}
-                        </h4>
-                        <h5 className="panel-rating">
-                            <p>
-                                <i className="fas fa-heart"></i>
-                                &nbsp;
-                                <span>{this.props.data.vote_average}</span>
-                            </p>
-                        </h5>
-                    </div>
-
-                    <MovieModal
-                        handleOpenModal={this.handleOpenModal} 
-                        handleCloseModal={this.handleCloseModal} 
-                        showModal={this.state.showModal}
-                        video={this.state.video}
-                        data={this.props.data}
-                    />
-                </div>
-            </Fragment>
-        );
-    }
-}
 
 
-
-
-
-
-/*
 export default ({data}) => { 
     const [selectedMovie, setSelectedMovie] = useState(undefined);
     const [video, setVideo] = useState(YOUTUBE_LINK);
 
     useEffect(() => {
         const get_movie_call = async () => {
-            const movie = await fetch(`${BASE_URL}/movie/${this.props.data.id}/videos${API_KEY}&language=en-US`);
+            const movie = await fetch(`${BASE_URL}/movie/${data.id}/videos${API_KEY}&language=en-US`);
             const json = await movie.json();
 
             setVideo(`${video}${json.results[0].key}`);
@@ -100,16 +27,16 @@ export default ({data}) => {
                 className='panel' 
                 onClick={() => setSelectedMovie(data)}
             >
-                <img className="panel-img" src={`${IMG_URL}` + this.props.data.poster_path} alt={this.props.data.title} />
+                <img className="panel-img" src={`${IMG_URL}` + data.poster_path} alt={data.title} />
                 <div className="panel-body">
                     <h3 className='panel-title'>
-                        { (this.props.data.title || this.props.data.original_name).length >= 24 
-                            ? (this.props.data.title || this.props.data.original_name).slice(0, 24).concat('...') 
-                            : this.props.data.title || this.props.data.original_name }
+                        { (data.title || data.original_name).length >= 24 
+                            ? (data.title || data.original_name).slice(0, 24).concat('...') 
+                            : data.title || data.original_name }
                     </h3>
                     <h4 className="panel-genres">
                         {gen.map (genre => (
-                            this.props.data.genre_ids.includes(genre.id) 
+                            data.genre_ids.includes(genre.id) 
                                 ? `${genre.name}\xa0\xa0` 
                                 : null
                         ))}
@@ -118,7 +45,7 @@ export default ({data}) => {
                         <p>
                             <i className="fas fa-heart"></i>
                             &nbsp;
-                            <span>{this.props.data.vote_average}</span>
+                            <span>{data.vote_average}</span>
                         </p>
                     </h5>
                 </div>
@@ -132,5 +59,5 @@ export default ({data}) => {
         </Fragment>
     );
 }
-*/
+
 
--- src/components/movies_list/MovieModal.jsx
+++ src/components/movies_list/MovieModal.jsx
@@ -4,46 +4,27 @@ import Modal from 'react-modal';
 
 export default (props) => (
     <div>
-        <Modal 
-            isOpen={props.showModal}
-            contentLabel="onRequestClose Example"
-            onRequestClose={props.handleCloseModal}
-            >
-            <div className='container'>
-                <h1>{props.data.title}</h1>
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe 
-                        class="embed-responsive-item" 
-                        src={props.video}
-                        allowfullscreen
-                    ></iframe>
+        <Modal
+            isOpen={!!props.selectedMovie}
+            contentLabel='Selected Movie'
+            ariaHideApp={false}
+            onRequestClose={props.closeModal}
+            shouldCloseOnOverlayClick={true}
+        >
+           {props.selectedMovie &&  (
+                <div className='container'>
+                    <h1>{props.selectedMovie.title}</h1>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe 
+                            class="embed-responsive-item" 
+                            src={props.video}
+                            allowfullscreen
+                        ></iframe>
+                    </div>
+                    <p className='lead'>{props.selectedMovie.overview}</p>
+                    <button onClick={props.closeModal}>Close</button>  
                 </div>
-                <p className='lead'>{props.data.overview}</p>
-                <button onClick={props.handleCloseModal}>Close Modal</button>
-            </div>
+           )}
         </Modal>
     </div>
 );
-
-/*<Modal
-    isOpen={!!selectedMovie}
-    contentLabel='Selected Movie'
-    ariaHideApp={false}
-    onRequestClose={closeModal}
-    shouldCloseOnOverlayClick={true}
->
-   {selectedMovie &&  (
-        <div className='container'>
-            <h1>{selectedMovie.title}</h1>
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe 
-                    class="embed-responsive-item" 
-                    src={video}
-                    allowfullscreen
-                ></iframe>
-            </div>
-            <p className='lead'>{selectedMovie.overview}</p>
-            <button onClick={closeModal}>Close</button>  
-        </div>
-   )}
-</Modal>*/
