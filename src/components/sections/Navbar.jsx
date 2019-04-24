5501986af4fd39b58c48c230fd50a98c0f893e50
Cristi Tarasi <tarasicristia@gmail.com>
2019-04-24 19:06:58 +0100

    created 5th branch to safely make changes to the carousel components

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default () => (
-    <nav class="navbar navbar-expand-lg m-0">
+    <nav className="navbar navbar-expand-lg m-0">
         <div className='container'>
-            <NavLink class="navbar-brand" to='/'>
+            <NavLink className="navbar-brand" to='/'>
                 <img src='../../images/mdb.png' />
             </NavLink>
 
             <button 
-                class="navbar-toggler" type="button" data-toggle="collapse" 
+                className="navbar-toggler" type="button" data-toggle="collapse" 
                 data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" 
                 aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
                         <NavLink exact activeClassName='is-active' to='/'>
                             Home
-                            <span class="sr-only">(current)</span>
+                            <span className="sr-only">(current)</span>
                         </NavLink>
                     </li>
 
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink exact activeClassName='is-active' to='/kids'>
                             Kids
                         </NavLink>
                     </li>
 
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink exact activeClassName='is-active' to='/tv'>
                             TV
                         </NavLink>
